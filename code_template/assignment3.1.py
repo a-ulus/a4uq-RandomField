2f483b445ee368aeb75d4e4fa8ea759263b12990
Daria <d.al.tarasova@gmail.com>
2023-07-12 14:05:22 +0200

    eigenvalues calculated

--- code_template/assignment3.1.py
+++ code_template/assignment3.1.py
@@ -15,13 +15,46 @@ def WP_KL_approx(zeta, t, M):
 	return W
 
 
+def karhunen_loeve_eigenvalues(M):
+	# Generate a random covariance matrix
+	covariance_matrix = np.random.rand(M, M)
+	covariance_matrix = (covariance_matrix + covariance_matrix.T) / 2  # Make it symmetric
+
+	# Compute the eigenvalues of the covariance matrix
+	eigenvalues = np.linalg.eigvalsh(covariance_matrix)
+
+	return eigenvalues
+
+
 if __name__ == '__main__':
 	# the two sizes mentioned in the worksheet
 	N = 1000
 	M = [10, 100, 1000]
 	
 	dt = 1./N
-	t 	= np.range(0, 1+dt, dt)
+	t 	= np.arange(0, 1+dt, dt)
+
+	# Parameters
+	m = 1000
+
+	# Compute eigenvalues
+	eigenvalues = karhunen_loeve_eigenvalues(m)
+	print(eigenvalues)
+	eigenvalues_wo_outliers = [item for item in eigenvalues if item< 100]
+
+	# Plot the eigenvalues
+	plot(eigenvalues, 'bo')
+	xlabel('Eigenvalue index')
+	ylabel('Eigenvalue')
+	title('Eigenvalues of Karhunen-Loeve Expansion')
+	show()
+
+	# Plot the eigenvalues without outliers
+	plot(eigenvalues_wo_outliers, 'bo')
+	xlabel('Eigenvalue index')
+	ylabel('Eigenvalue')
+	title('Eigenvalues without outliers of Karhunen-Loeve Expansion')
+	show()
 
 	# first, use the standard defition to generate a realization with N samples
 
